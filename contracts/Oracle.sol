54fdd58ece8c626c7fa46713d967dbaa3c4b49b1
Hadi Mahdavi <expandboard@users.noreply.github.com>
2024-11-27 11:50:42 +0330

    Separate contracts

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -4,27 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
-contract OracleFactory {
-    address public immutable governanceToken;
-    address[] public oracles;
-
-    event OracleCreated(address oracle);
-
-    constructor(address _governanceToken) {
-        governanceToken = _governanceToken;
-    }
-
-    function createOracle() external returns (address) {
-        Oracle oracle = new Oracle(governanceToken, msg.sender);
-        oracles.push(address(oracle));
-        emit OracleCreated(address(oracle));
-        return address(oracle);
-    }
-
-    function getOracles() external view returns (address[] memory) {
-        return oracles;
-    }
-}
 
 contract Oracle is Ownable {
     IERC20 public immutable governanceToken;
